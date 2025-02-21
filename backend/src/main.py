debe5909bf4acdbc5f7f02a874f7159e6b8490cd
ckcs_yozen <yozen0405@gmail.com>
2025-02-21 14:33:59 +0800

    fix: external connection issue

--- backend/src/main.py
+++ backend/src/main.py
@@ -20,9 +20,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        os.getenv("VITE_BASE_URL", "http://localhost")
-    ],  
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],  
     allow_headers=["*"], 
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 // src/config.js
-const BASE_URL = import.meta.env.VITE_BASE_URL || "http://localhost";
+const BASE_URL = import.meta.env.VITE_BASE_URL || `${window.location.origin}`;
 const WS_URL = BASE_URL.replace(/^http/, "ws").replace(/\/$/, ""); 
 
 export { BASE_URL, WS_URL };
